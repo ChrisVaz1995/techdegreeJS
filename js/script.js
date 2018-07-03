804a95c8dd233fe7792998f8ad962c182ae22a25
Christopher Vazquez <christopher.vazquez2015@gmail.com>
2018-07-03 09:02:45 -0500

    Project 1 FullStack JS
    This is for project 1 for the Full Stack Javascript TD. I've done one for the FEWD project, but now I am doing it for the full stack javascript TD.

--- js/script.js
+++ js/script.js
@@ -3,30 +3,33 @@ var quotes = [
 {
   quote: "Change is the law of life. And those who look only to the past or present are certain to miss the future.",
   source: "John F. Kennedy",
-  citation: "White House",
-  year: "Unkown"
+  citation: " White House",
+  year: " Unkown",
+  category: " U.S."
 },
 
 {
   quote: "Not everyone who works hard is rewarded. But! All those who succeed have worked hard!",
   source: "Kamogawa Genji",
   citation: "Hajime no Ippo: The Fighting",
-  year: 2013
+  year: 2013,
+  category: " Anime"
 },
 
 {
   quote: "There is beauty in simplicity",
   source: "Paul Nakauchi",
-  citation: "Overwatch",
-  year: "2016"
+  citation: " Overwatch",
+  year: "2016",
+  category: " Video Game"
 },
 
 {
   quote:"I used to follow my dreams until I caught up to em'",
   source: "Logic",
-  citation: "Undeniable Album",
-  year: 2012
-
+  citation: " Undeniable Album",
+  year: 2012,
+  category: " Music"
 }
 
 
@@ -44,6 +47,7 @@ function printQuote() {
   var message = "<p class='quote'>" + currentQuote.quote + "</p>";
   message += "<p class='source'>" + currentQuote.source + "</p>";
   message += "<span class='citation'>" + currentQuote.citation + "</span>";
+  message += "<span class='category'>" + currentQuote.category + "</span>";
 
   if(currentQuote.year != undefined) {
     message += "<span class='year'>" + currentQuote.year + "</span>";
@@ -52,5 +56,14 @@ function printQuote() {
   document.getElementById("quote-box").innerHTML = message;
 }
 
+function randomColors() {
+  var addColor1 = Math.floor(Math.random() * 256);
+  var addColor2 = Math.floor(Math.random() * 256);
+  var addColor3 = Math.floor(Math.random() * 256);
+  var combineColors = "rgb(" + addColor1 + "," + addColor2 + "," + addColor3 + ")"
+  console.log(combineColors);
+  document.body.style.background = combineColors;
+}
 
+randomColors();
 document.getElementById('loadQuote').addEventListener('click', printQuote, false);
